998c34afc9c160e27aca3acec33fb9099daec2d3
Johannes <jomi@einer.org>
2018-05-12 18:16:31 +0200

    Style for range function.

--- src/range.js
+++ src/range.js
@@ -1,11 +1,11 @@
 function range(min, max=null, step=1) {
   if(max == null) {
     max = min;
-    min = 0
+    min = 0;
   }
   return Array.apply(null, Array(Math.floor((max-min)/Math.abs(step)))).map(
     (x, i) => min + i*step
-  )
+  );
 }
 
-module.exports = range
+module.exports = range;
